fb8db2c6db43d16b166dc0c2157548d85801218e
adighosh18 <112495570+adighosh18@users.noreply.github.com>
2023-12-05 14:37:10 -0500

    Update train_model.py

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,6 +1,7 @@
 from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, Reshape
 from tensorflow.keras.models import Model
 import tensorflow as tf
+from tensorflow.keras.layers import Dense, Flatten
 
 input_layer = Input(shape=(784,))
 
@@ -36,7 +37,9 @@ def encoder_decoder(no_of_encoder_layers, no_of_decoder_layers, activation_funct
         x = UpSampling2D((2, 2))(x)
         x = Conv2D(32, (3, 3), activation=activation_function, padding='same')(x)
         x = UpSampling2D((2, 2))(x)
-        decoded = x
+        x = Flatten()(x)
+        decoded = Dense(784, activation='sigmoid')(x)
+        #decoded = x
         return decoded
 
     elif no_of_decoder_layers == 5:
@@ -44,7 +47,9 @@ def encoder_decoder(no_of_encoder_layers, no_of_decoder_layers, activation_funct
         x = UpSampling2D((2, 2))(x)
         x = Conv2D(32, (3, 3), activation=activation_function, padding='same')(x)
         x = UpSampling2D((2, 2))(x)
-        decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+        #decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+        x = Flatten()(x)
+        decoded = Dense(784, activation='sigmoid')(x)
         return decoded
 
     elif no_of_decoder_layers == 6:
@@ -54,7 +59,9 @@ def encoder_decoder(no_of_encoder_layers, no_of_decoder_layers, activation_funct
         x = UpSampling2D((2, 2))(x)
         x = Conv2D(32, (3, 3), activation=activation_function, padding='same')(x)
         x = UpSampling2D((2, 2))(x)
-        decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+        #decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+        x = Flatten()(x)
+        decoded = Dense(784, activation='sigmoid')(x)
         return decoded
 
 
@@ -79,6 +86,15 @@ def model(no_of_encoder_layers, no_of_decoder_layers, activation_function):
 def train_model(x_train_noise, X_train, x_test_noise, X_test, no_of_encoder_layers, no_of_decoder_layers,
                 activation_function, no_of_epochs):
     autoencoder = model(no_of_encoder_layers, no_of_decoder_layers, activation_function)
-    autoencoder.fit(x_train_noise, X_train, epochs=no_of_epochs, batch_size=128,
-                    validation_data=(x_test_noise, X_test))
+    if x_train_noise.shape[1] == 28 and x_train_noise.shape[2] == 28:
+        x_train_noise = x_train_noise.reshape(x_train_noise.shape[0], x_train_noise.shape[1]*x_train_noise.shape[2])
+    if x_test_noise.shape[1] == 28 and x_test_noise.shape[2] == 28:
+        x_test_noise = x_test_noise.reshape(x_test_noise.shape[0], x_test_noise.shape[1]*x_test_noise.shape[2])
+    if X_train.shape[1] == 28 and X_train.shape[2] == 28:
+        X_train = X_train.reshape(X_train.shape[0], X_train.shape[1]*X_train.shape[2])
+    if X_test.shape[1] == 28 and X_test.shape[2] == 28:
+        X_test = X_test.reshape(X_test.shape[0], X_test.shape[1]*X_test.shape[2])
+    #x_train_noise = x_train_noise.reshape(x_train_noise.shape[0], x_train_noise.shape[1]*x_train_noise.shape[2])
+    #X_train = X_train.reshape(X_train.shape[0], X_train.shape[1]*X_train.shape[2])
+    autoencoder.fit(x_train_noise, X_train, epochs=no_of_epochs, batch_size=128, validation_data=(x_test_noise, X_test))
     return autoencoder
